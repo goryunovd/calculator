4350e5d150b1441e8ff734189e327359d83e4f74
Горюнов Дмитрий <dsgoryunov@stud.etu.ru>
2021-12-13 20:00:27 +0300

    add more features

--- calculator/calculator.cpp
+++ calculator/calculator.cpp
@@ -3,29 +3,10 @@
 #include<string>
 #include <iostream>
 #include"stack.h"
-#include<ctime>
 #define pi 3.14159265 
 #define exp 2.718281
 using namespace std;
-int priority(string c)
-{
-	if (c == "^")
-	{
-		return 3;
-	}
-	else if (c == "*" || c == "/")
-	{
-		return 2;
-	}
-	else if (c == "+" || c == "-")
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
-}
+
 int check_for_correct_input(string str, bool check_for_bracekts)
 {
 	check_for_bracekts = 1;
@@ -60,16 +41,7 @@ int stack_brackets_check(string str, bool check)
 	}
 	if (stck.empty() == 0) { check = 1; }
 	return check;
-};/*
-void make_colculation_to_stack(Stack<string> *stck, string str)
-{//sqrt,cos,sin,tg,ctg,ln,log,sgn
-	stck->push(")");
-	for(int i = 0; i < str.length(); i++)
-	{
-		if(str[i]=='('){stck->push}
-	}
-};*/
-
+};
 void transform_inf_to_stack(Stack<string> *stck, string str)
 {
 	stck->push(")");
@@ -77,58 +49,92 @@ void transform_inf_to_stack(Stack<string> *stck, string str)
 	int pos,j;
 	for (int i = 0; i < str.length(); i++)
 	{
-	if (str[i] == '(') { stck->push(")"); }
-	if (str[i] == '(') { stck->push("("); }
+	if (str[i] == '(') { stck->push(")");}else
+	if (str[i] == ')') { stck->push("(");}else
 	//sqrt,sgn,sin,cos,log,ln,tg,ctg
 	if (str[i] == 's'&& str[i + 1] == 'q') 
-	{pos = str.find(")", i); tmp_str.assign(str, i, pos - i); stck->push(tmp_str); tmp_str = "\0"; i = ++pos;	}//sqrt
-	
+	{pos = str.find(")", i); tmp_str.assign(str, i, pos - i+1); stck->push(tmp_str); tmp_str = "\0"; i = pos;		}//sqrt
+	else
 	if (str[i] == 's'&& str[i + 1] == 'g') 
-	{ pos = str.find(")", i); tmp_str.assign(str, i, pos - i); stck->push(tmp_str); tmp_str = "\0"; i = ++pos;	}//sgn
-
-	if (str[i] == 's'&& str[i + 1] == 'g')
-	{pos = str.find(")", i); tmp_str.assign(str, i, pos - i); stck->push(tmp_str); tmp_str = "\0"; i = ++pos;	}//sgn
-	
+	{ pos = str.find(")", i); tmp_str.assign(str, i, pos -i +1); stck->push(tmp_str); tmp_str = "\0"; i = pos;		}//sgn
+	else
+	if (str[i] == 's'&& str[i + 1] == 'g')	
+	{pos = str.find(")", i); tmp_str.assign(str, i, pos -i +1); stck->push(tmp_str); tmp_str = "\0"; i = pos;		}//sgn
+	else
 	if (str[i] == 's'&& str[i + 1] == 'i')
-	{pos = str.find(")", i); tmp_str.assign(str, i, pos - i); stck->push(tmp_str); tmp_str = "\0"; i = ++pos;	}//sin
-	
+	{pos = str.find(")", i); tmp_str.assign(str, i, pos -i +1); stck->push(tmp_str); tmp_str = "\0"; i = pos;		}//sin
+	else
 	if (str[i] == 'c'&& str[i + 1] == 'o')
-	{pos = str.find(")", i); tmp_str.assign(str, i, pos - i); stck->push(tmp_str); tmp_str = "\0"; i = ++pos;	}//cos
-	
+	{		pos = str.find(")", i); tmp_str.assign(str, i, pos - i + 1); stck->push(tmp_str); tmp_str = "\0"; i = pos;}//cos
+	else
 	if (str[i] == 't'&& str[i + 1] == 'g')
-	{	pos = str.find(")", i); tmp_str.assign(str, i, pos - i); stck->push(tmp_str); tmp_str = "\0"; i = ++pos;}//tg
-	
+	{		pos = str.find(")", i); tmp_str.assign(str, i, pos - i + 1); stck->push(tmp_str); tmp_str = "\0"; i = pos;}//tg
+	else
 	if (str[i] == 'c'&& str[i + 1] == 't')
-	{pos = str.find(")", i); tmp_str.assign(str, i, pos - i); stck->push(tmp_str); tmp_str = "\0"; i = ++pos;	}//ctg
-	
+	{pos = str.find(")", i); tmp_str.assign(str, i, pos -i +1); stck->push(tmp_str); tmp_str = "\0"; i = pos;		}//ctg
+	else
 	if (str[i] == 'l'&& str[i + 1] == 'o')
-	{pos = str.find(")", i); tmp_str.assign(str, i, pos - i); stck->push(tmp_str); tmp_str = "\0"; i = ++pos;	}//log
-	
+	{pos = str.find(")", i); tmp_str.assign(str, i, pos -i +1); stck->push(tmp_str); tmp_str = "\0"; i = pos;		}//log
+	else
 	if (str[i] == 'l'&& str[i + 1] == 'n')
-	{pos = str.find(")", i); tmp_str.assign(str, i, pos - i); stck->push(tmp_str); tmp_str = "\0";	i = ++pos;	}//ln
-	//double
+	{pos = str.find(")", i); tmp_str.assign(str, i, pos -i +1); stck->push(tmp_str); tmp_str = "\0";	i = pos;	}//ln
+	else
+																													 //double
 	if (str[i] >= 48 && str[i] <= 57) 
 	{
-		j = i; while (str[i] == 46 || (str[i] >= 48 && str[i] <= 57)) 
-		{	i++; tmp_str.assign(str, j, i-j); stck->push(tmp_str); tmp_str = "\0";} 
+		j = i; while (str[i] == 46 || (str[i] >= 48 && str[i] <= 57)){i++;} 
+		tmp_str.assign(str, j, i - j); stck->push(tmp_str); tmp_str = "\0"; i--;
 	}
+	else
 	// + * - / ^ 
 	if (str[i] == '+') { stck->push("+"); }
+	else
 	if (str[i] == '*') { stck->push("*"); }
+	else
 	if (str[i] == '-') { stck->push("-"); }
+	else
 	if (str[i] == '/') { stck->push("/"); }
+	else
 	if (str[i] == '^') { stck->push("^"); }
-	
-	
+	else
+	//pi and exp
+	if (str[i] == 'p'&&str[i] == 'i') { stck->push("pi"); i++; 		}
+	else
+	if (str[i] == 'e'&&str[i] == 'p') { stck->push("exp"); i += 2; 	}
+
 	}
 
 	stck->push("(");
 };
-void prefix_notation_isnt_my_oreintation(Stack<string> *pref_stck, string str)
+int priority(string c)
+{	
+	if (c == "^")					{		return 3;	}
+	else if (c == "*" || c == "/")	{		return 2;	}
+	else if (c == "+" || c == "-")	{		return 1;	}
+	else							{		return 0;	}
+}
+void prefix_notation_isnt_my_oreintation(Stack<string> *prefix, Stack<string> str)
 {
+	/*
+	stack string| stack operators |stack prefix
+	*/
 	Stack<string> operators;
-	Stack<string> outputstring;
-	
+	string tmp;
+	while (!str.empty())
+	{
+		tmp = str.top();
+		
+			if (tmp == "(") { operators.push(tmp); str.pop(); }
+			if (tmp == ")") { while (tmp != "(") { tmp = operators.top(); if (tmp != "(") { prefix->push(tmp); }operators.pop(); } }
+//			if (tmp == "*" || tmp == "+" || tmp == "-" || tmp == "/" || tmp == "^")
+			if (tmp == "*") { if (priority(tmp) <= priority(operators.top()))  {/*} if (operators.top == "*" || operators.top == "/" || operators.top == "^") {  */prefix->push(operators.top()); operators.pop();} }
+			if (tmp == "+") { if (priority(tmp) <= priority(operators.top())) {/*} if (operators.top == "*" || operators.top == "/" || operators.top == "^") {  */prefix->push(operators.top()); operators.pop(); } }
+			if (tmp == "-") { if (priority(tmp) <= priority(operators.top())) {/*} if (operators.top == "*" || operators.top == "/" || operators.top == "^") {  */prefix->push(operators.top()); operators.pop(); } }
+			if (tmp == "/") { if (priority(tmp) <= priority(operators.top())) {/*} if (operators.top == "*" || operators.top == "/" || operators.top == "^") {  */prefix->push(operators.top()); operators.pop(); } }
+			if (tmp == "^") { if (priority(tmp) <= priority(operators.top())) {/*} if (operators.top == "*" || operators.top == "/" || operators.top == "^") {  */prefix->push(operators.top()); operators.pop(); } }
+			if (tmp != "*" && tmp != "+" && tmp != "-" && tmp != "/" && tmp != "^" && tmp != "(" && tmp != ")")
+			{prefix->push(tmp); str.pop();}
+	}
 };
 /*
 1) Create empty stack for saving operators
@@ -145,6 +151,7 @@ otherway add token in stack firstly
 int main()
 {
 	Stack<string> prefix_stack;
+	Stack<string> from_str;
 	string str = "((2.2+1.1*4.4*1.0)*5.5)/6.6+(cos(0.123891298)*2.0+sin(0.0))^2.0+sgn(-5.0)+log(exp)+log(1.0)";
 	bool check_for_bracekts = 0;
 	cout << "our txt is :" << str << endl;
@@ -157,6 +164,8 @@ int main()
 
 
 	//prefix_isnt_my_oreintation()
-
-
+	transform_inf_to_stack(&from_str, str);
+	while (!from_str.empty()) { cout << from_str.top(); from_str.pop(); }
+	prefix_notation_isnt_my_oreintation(&prefix_stack, from_str);
+	while (!prefix_stack.empty()) { cout << prefix_stack.top(); prefix_stack.pop(); }
 }
